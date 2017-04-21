3c87aea1d321dbbd97af80a141cf9e214a42c445
Exund <SylverProfessionnel@gmail.com>
2017-04-21 22:50:57 +0200

    ID verification

--- AutomatronBlockSpawn/ActionBlockSpawner.cs
+++ AutomatronBlockSpawn/ActionBlockSpawner.cs
@@ -39,9 +39,14 @@ namespace Sylver.AutomatronExtention
         {
             if (Game.IsSimulating) return;
 
-            if (blockID != "")
+            try
             {
-                this.Title = "Spawn " + PrefabMaster.BlockPrefabs[valueID].blockBehaviour.gameObject.GetComponent<MyBlockInfo>().blockName;
+                valueID = int.Parse(blockID);
+                Title = "Spawn " + PrefabMaster.BlockPrefabs[valueID].blockBehaviour.gameObject.GetComponent<MyBlockInfo>().blockName;
+            }
+            catch
+            {
+                Title = "Invalid ID";
             }
         }
 
@@ -129,9 +134,9 @@ namespace Sylver.AutomatronExtention
 							break;
 					}
 					GUILayout.BeginHorizontal();
-					GUILayout.Label("   X");
-					GUILayout.Label("   Y");
-					GUILayout.Label("   Z");
+					GUILayout.Label("X");
+					GUILayout.Label("Y");
+					GUILayout.Label("Z");
 					GUILayout.EndHorizontal();
 					GUILayout.BeginHorizontal();
 				}
